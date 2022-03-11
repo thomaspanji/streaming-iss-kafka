8d3824eaad943e8811145a93eae37eea322c386c
thomaspanji <thopan.sakti@gmail.com>
2022-03-11 12:41:00 +0700

    Initial commit

---
+++ kafka_producer.py
@@ -0,0 +1,19 @@
+import json
+import requests
+from kafka import KafkaProducer
+from time import sleep
+
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost','9092'],
+    value_serializer=lambda x: json.dumps(x).encode('utf-8')
+)
+
+for _ in range(20):
+    res = requests.get('http://api.open-notify.org/iss-now.json')
+    data = json.loads(res.content.decode('utf-8'))
+    print(data)
+    producer.send('iss-location', value=data)
+    sleep(5)
+    producer.flush()
+    
