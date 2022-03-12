1211fb3712bee5b3bd3bfec0293f8b8c5116c936
thomaspanji <thopan.sakti@gmail.com>
2022-03-12 19:16:08 +0700

    Change logic to run continuosly

--- kafka_producer.py
+++ kafka_producer.py
@@ -9,11 +9,19 @@ producer = KafkaProducer(
     value_serializer=lambda x: json.dumps(x).encode('utf-8')
 )
 
-for _ in range(20):
+# for _ in range(20):
+#     res = requests.get('http://api.open-notify.org/iss-now.json')
+#     data = json.loads(res.content.decode('utf-8'))
+#     print(data)
+#     producer.send('iss-location', value=data)
+#     producer.flush()
+#     sleep(10)\
+
+while True:
     res = requests.get('http://api.open-notify.org/iss-now.json')
     data = json.loads(res.content.decode('utf-8'))
     print(data)
     producer.send('iss-location', value=data)
-    sleep(5)
     producer.flush()
+    sleep(10)
     
