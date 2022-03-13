2f4e00a31f957d9a43c4ddca2a286fb191864c01
thomaspanji <thopan.sakti@gmail.com>
2022-03-13 21:58:29 +0700

    Add description for argparse

--- kafka_producer_ver2.py
+++ kafka_producer_ver2.py
@@ -7,7 +7,10 @@ STREAM_MINUTE = 0
 
 
 parser = argparse.ArgumentParser(
-    description='Streaming ISS data for certain time.'
+    description="""
+    Streaming ISS position data for certain time.
+    If no argument supplied, the stream will run forever.
+    """
 )
 group = parser.add_mutually_exclusive_group()
 group.add_argument(
@@ -16,9 +19,9 @@ group.add_argument(
     type=int,
     help="""
     Specify a number of orbit you want to fetch the data.
-    
     Time to take an orbit is approximately 90-93 minutes,
     but we are rounding it to 95 minutes.
+    So, argument `--orbit 2` will stream the data for 190 minutes.
     """ 
 )
 group.add_argument(
@@ -39,6 +42,3 @@ else:
     len_stream = None
 
 run(len_stream)
-
-if args.help:
-    parser.print_help()
